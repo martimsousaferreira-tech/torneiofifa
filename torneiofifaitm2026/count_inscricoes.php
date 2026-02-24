6dd9e90eda41b9ef9dea445c55a38fd02f58551c
martimsf <martimsousaferreira@gmail.com>
2026-02-24 16:36:41 +0000

    a

--- torneiofifaitm2026/count_inscricoes.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once "db_connect.php";
-$res = $conn->query("SELECT COUNT(*) as total FROM inscricoes");
-echo "Total inscricoes: " . $res->fetch_assoc()['total'] . "\n";
-?>
--- torneiofifaitm2026/logout_admin.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['admin_auth']);
-header("Location: backoffice.php");
-exit;
-?>
